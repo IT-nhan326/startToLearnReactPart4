add1422bfbd0a701a706f43f6041a34be34e9a83
IT-nhan326 <nhan.plt2603@gmail.com>
2021-08-22 14:13:02 +0700

    useRef-useReducer-useEffect-Context-forwardRef

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React, { useState } from 'react';
+import React, { useContext } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./context/auth-context";
+
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
-
+  const Ctx = useContext(AuthContext)
+  
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+        <MainHeader onLogout={Ctx.onLogout} />
+        <main>
+          {!Ctx.isLoggedIn && <Login />}
+          {Ctx.isLoggedIn && <Home />}
+        </main>
     </React.Fragment>
   );
 }
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../context/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+const Navigation = () => {
+  const Ctx = useContext(AuthContext)
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {Ctx.isLoggedIn && (
+                <li>
+                  <a href='/'>Users</a>
+                </li>
+              )}
+              {Ctx.isLoggedIn && (
+                <li>
+                  <a href='/'>Admin</a>
+                </li>
+              )}
+              {Ctx.isLoggedIn && (
+                <li>
+                  <button onClick={Ctx.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        );
 };
 
 export default Navigation;
